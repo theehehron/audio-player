e441d5cd1c9d7e2618c5ebd0ffeb1a40ccd80ce1
Aaron Greisen <aaron.greisen1@gmail.com>
2025-09-13 12:41:03 -0700

    Initial Commit

---
+++ MP3Player/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MP3Player
+//
+//  Created by Aaron Greisen on 9/13/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
